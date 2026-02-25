4dd06c7766486483646fab31da71ecb5d4e8cd5a
Plexome-Architect <architect@plexome.ai>
2026-02-26 00:07:27 +0200

    Update module_interface.h

--- include/module_interface.h
+++ include/module_interface.h
@@ -1,1 +1,31 @@
+#ifndef PXM_MODULE_INTERFACE_H
+#define PXM_MODULE_INTERFACE_H
 
+#include "plexome_types.h"
+#include "pxm_platform.h"
+
+/**
+ * Common return codes for all Plexome modules
+ */
+enum class PxmStatus {
+    OK = 0,
+    ERROR_INIT_FAILED = 1,
+    ERROR_MODULE_NOT_FOUND = 2,
+    ERROR_INCOMPATIBLE_VERSION = 3
+};
+
+/**
+ * Module information structure for the Host
+ */
+struct PxmModuleInfo {
+    const char* name;
+    const char* version;
+    const char* description;
+};
+
+// Standard function pointer types for dynamic loading
+typedef PxmModuleInfo (*PXM_GET_INFO_FUNC)();
+typedef PxmStatus (*PXM_INIT_FUNC)(const PxmConfig* config);
+typedef void (*PXM_SHUTDOWN_FUNC)();
+
+#endif // PXM_MODULE_INTERFACE_H
